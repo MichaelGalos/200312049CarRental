13ffd8b7506b25862c76d5eb022f0a694b789d11
Michael Galos <michael.a.galos@gmail.com>
2019-05-27 19:17:30 -0400

    First Commit

---
+++ 200312049CarRental/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_200312049CarRental.Startup))]
+namespace _200312049CarRental
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
